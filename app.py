860e7378243deea967946588b7433a0175bd0a2b
Manigandan <175315588+ManigandanCR-7@users.noreply.github.com>
2026-09-05 17:17:48 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/health")
+def health():
+    return {
+        "status": "running",
+        "service": "Voice Cursor"
+    }
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
+
